dd6f1f519371b9e8a0c68b31c6df6555f09ceb5f
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-07-09 15:43:36 -0600

    Added menu open/close animations.

--- js/code.js
+++ js/code.js
@@ -1,15 +1,46 @@
+var menuOpen = false;
+var mobile = false;
+
+function closeMenu() {
+    menuOpen = false;
+    $('#menu-container').slideUp('slow', function() {});
+}
+
+function openMenu() {
+    menuOpen = true;
+    $('#menu-container').slideDown('slow', function() {});
+}
+
 function setup() {
+    if(window.innerWidth < 992) {
+        mobile = true;
+    }
+
     $('.scrollLink').click(function(e) {
         e.preventDefault();
         var navHeight = $('#navbar')[0].offsetHeight;
+        var menuContainerHeight = $('#menu-container')[0].offsetHeight;
 
         var id = this.href.split('#')[1];
-        var position = $('#'+id)[0].offsetTop - navHeight;
+        var position = $('#'+id)[0].offsetTop - navHeight + menuContainerHeight;
 
         $('html,body').animate({
             scrollTop: position
         },
         'slow');
+
+        if(menuOpen) {
+            closeMenu();
+        }
+    });
+
+    $('#menu-btn').click(function(e) {
+        e.preventDefault();
+        if(menuOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
     });
 }
 
