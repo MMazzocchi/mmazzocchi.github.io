8aa3a1dcb4d97e680ad2fb4cc855e4bbeee80fcc
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-27 09:23:34 -0400

    Moved nav stuff into Nav.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
-import {
-  Collapse,
-  Container,
-  Nav,
-  NavItem,
-  NavLink,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler
-} from "reactstrap";
+import { Container } from "reactstrap";
+import { Nav } from "./Nav.js";
+
+
+const links = [
+  { text: "Home",    dest: "/" },
+  { text: "Contact", dest: "/" },
+];
 
 const App = () => (
   <Container>
-    <Navbar expand="md">
-      <NavbarBrand href="/">
-        Max Mazzocchi
-      </NavbarBrand>
-      <NavbarToggler />
-      <Collapse navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="/">Home</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <Nav links={ links } />
   </Container>
 );
 
---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Collapse,
+  Container,
+  Nav,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler
+} from "reactstrap";
+
+const MyNavItem = ({ dest, text }) => (
+  <NavItem>
+    <NavLink href={ dest }>
+      { text }
+    </NavLink>
+  </NavItem>
+);
+
+const MyNav = ({ links }) => (
+  <Navbar expand="md">
+    <NavbarBrand href="/">
+      Max Mazzocchi
+    </NavbarBrand>
+    <NavbarToggler />
+    <Collapse navbar>
+      <Nav className="ml-auto" navbar>
+        { links.map((link) => (
+          <MyNavItem { ...link } key={ `navitem-${link.text}` } />
+        ))}
+      </Nav>
+    </Collapse>
+  </Navbar>
+);
+
+
+export { MyNav as Nav };
