df374c8d309ce6ec7929770d3d6f2f4e13465127
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-27 10:23:06 -0400

    Added simple header to main page.

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ const links = [
 const App = () => (
   <Container>
     <Nav links={ links } />
-    <Row>
+    <Row className="align-items-center">
       <Col md="6">
         <img src="images/me.jpg"
              className="rounded-circle img-fluid mx-auto d-block portrait" />
       </Col>
       <Col md="6">
+        <Row className="border-bottom">
+          <Col>
+            <h1>Max Mazzocchi</h1>
+            <p>Software Developer, DC Metro Area</p>
+          </Col>
+        </Row>
       </Col>
     </Row>
   </Container>
