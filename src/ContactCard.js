798c15edb763a4398104022df91193fc133d4f9a
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-28 09:43:38 -0400

    Modified styling on ContactCard.

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Row, Col, Card, CardDeck, CardBody, CardTitle, CardSubtitle, CardFooter, Button } from "reactstrap";
 
 const ContactCard = ({ icon, title, link, color }) => (
-  <Card className="text-center shadow"
+  <Card className="text-center shadow contact-card"
         tag="a" href={ link }>
-    <CardBody className="contact-card">
+    <CardBody>
       <CardTitle>
         <i className={ `${ icon } skill-icon` }
            style={{ "backgroundColor": color }}></i>
