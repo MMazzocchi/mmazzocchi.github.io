e788e78349e319bad6c8e0feab5e6ec3bb3ee8f3
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-28 08:55:40 -0400

    Added so basic content to Contact.

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Row, Col, Card, CardDeck, CardBody, CardTitle, CardSubtitle, CardFooter, Button } from "reactstrap";
 
 const Contact = () => (
-  <h1>Contact</h1>
+  <React.Fragment>
+    <Row className="align-items-center">
+      <Col md="4">
+        <h3>Let's talk!</h3>
+      </Col>
+      <Col md="8">
+        <CardDeck>
+          <Card className="text-center">
+            <CardBody>
+              <CardTitle>
+                <i className="fas fa-envelope skill-icon"
+                   style={{ "backgroundColor": "red" }}></i>
+              </CardTitle>
+              <CardSubtitle>
+                Email
+              </CardSubtitle>
+            </CardBody>
+            <CardFooter>
+              <Button block>
+                Go There
+              </Button>
+            </CardFooter>
+          </Card>
+        </CardDeck>
+      </Col>
+    </Row>
+  </React.Fragment>
 );
 
 export { Contact };
