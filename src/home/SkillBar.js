88832cb407b39b9138770b1a8a087013dc0007af
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-29 17:21:07 -0400

    Modified skillbar to be a class, only open one component at a time.

--- src/home/SkillBar.js
+++ src/home/SkillBar.js
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardText,
   Col,
+  Collapse,
   Row,
   UncontrolledCollapse
 } from "reactstrap";
@@ -23,34 +24,68 @@ const skills = [
   { icon: "ellipsis-h", label: "More",         color: "orange" }
 ];
 
-const SkillBar = () => (
-  <React.Fragment>
-    <Row className="justify-content-center">
-      { skills.map((skill) => (
-        <SkillIcon key={ `skill-icon-${ skill.icon }` }
-                   id={ `${ skill.icon }-toggler` } { ...skill } />
-      ))}
-    </Row>
-    <Row>
-      { skills.map((skill) => (
-        <UncontrolledCollapse key={ `${ skill.icon }-collapse` }
-                              toggler={ `#${ skill.icon }-toggler` }>
-          <Card className="shadow">
-            <CardHeader>
-              <Row className="justify-content-center">
-                <SkillIcon id={ `${ skill.icon }-header` } { ...skill } />
-              </Row>
-            </CardHeader>
-            <CardBody>
-              <CardText>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales iaculis ex eget commodo. Quisque non varius ex. Vivamus mollis accumsan tempus. Nam eget odio vestibulum odio consectetur gravida nec sit amet mi. Ut vehicula massa tellus, sed eleifend arcu pretium accumsan. Aliquam laoreet tortor vitae mi porttitor accumsan. Suspendisse malesuada quis risus et convallis. Aliquam sed ipsum et lacus ornare rhoncus in et augue. In quis dictum est, nec blandit ex. Vivamus sollicitudin ipsum at felis sagittis fringilla. 
-              </CardText>
-            </CardBody>
-          </Card>
-        </UncontrolledCollapse>
-      ))}
-    </Row>
-  </React.Fragment>
-);
+class SkillBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    skills.forEach((skill, index) => {
+      skill["id"] = "skill"+index;
+      this.state[skill.id] = false;
+    });
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(e) {
+    const target = e.currentTarget;
+    let state = {};
+
+    skills.forEach((skill) => {
+      state[skill.id] = false;
+    });
+
+    this.setState(state);
+
+    state[target.id] = !this.state[target.id];
+    this.setState(state);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Row className="justify-content-center">
+          { skills.map((skill) => (
+            <SkillIcon key={ `skill-icon-${ skill.icon }` }
+                       id={ skill.id }
+                       onClick={ this.toggle }
+                       { ...skill } />
+          ))}
+        </Row>
+        <Row>
+          { skills.map((skill) => (
+            <Collapse key={ `${ skill.icon }-collapse` }
+                      isOpen={ this.state[skill.id] }>
+              <Card className="shadow">
+                <CardHeader>
+                  <Row className="justify-content-center">
+                    <SkillIcon id={ `${ skill.icon }-header` } { ...skill } />
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <CardText>
+Lor    em ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales iaculis ex eget commodo. Quisque non varius ex. Vivamus mollis accumsan tempus. Nam eget odio vestibulum odio consectetur gravida nec sit amet mi. Ut vehicula massa tellus, sed eleifend arcu pretium accumsan. Aliquam laoreet tortor vitae mi porttitor accumsan. Suspendisse malesuada quis risus et convallis. Aliquam sed ipsum et lacus ornare rhoncus in et augue. In quis dictum est, nec blandit ex. Vivamus sollicitudin ipsum at felis sagittis fringilla. 
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Collapse>
+          ))}
+        </Row>
+
+      </React.Fragment>
+    );
+  }
+};
 
 export { SkillBar };
