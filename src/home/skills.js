3388749a5929dd8dc03fd4707b5da042bacacad1
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-30 18:07:51 -0400

    Added Desktop description.

--- src/home/skills.js
+++ src/home/skills.js
@@ -15,7 +15,10 @@ const skills = [
           development technologies and web design principles. Most recently,
           I've had the opportunity to work across several Django/React projects
           in my current job. In addition, I develop for the web in my spare time
-          on personal projects (including this site), mainly using NodeJS.
+          on personal projects (including this site), mainly using NodeJS. I
+          strive for solutions that work elegantly in many environments,
+          including desktop and mobile. I'm especially excited by recent
+          advancements in WebGL and 3D libraries like THREE.js.
         </CardText>
         <Row className="justify-content-center">
           {
@@ -31,7 +34,29 @@ const skills = [
       </CardBody>
     )
   },
-  { icon: "fas fa-desktop", label: "Desktop", color: "blue" },
+  { icon: "fas fa-desktop", label: "Desktop", color: "blue",
+    content: (
+      <CardBody>
+        <CardText>
+          My work in desktop and standalone applications has mainly been in my
+          work for various space systems companies. In these positions, my
+          experience spans a wide variety of languages and capabilities,
+          including modeling and simulation, ground processing applications,
+          databases, scripts, and more.
+        </CardText>
+        <Row className="justify-content-center">
+           {
+            ["Java", "C/C++", "Python", "Perl", "SQL" ].map((word, i) => (
+               <Col key={ `d-${i}` }
+                    className="text-center">
+                 <h5>{ word }</h5>
+               </Col>
+             ))
+          }
+        </Row>
+      </CardBody>
+    )
+  },
   { icon: "fas fa-wrench", label: "Tools", color: "grey" }, 
   { icon: "fas fa-flask", label: "Research", color: "purple" }, 
   { icon: "fas fa-redo", label: "Agile", color: "black" }, 
