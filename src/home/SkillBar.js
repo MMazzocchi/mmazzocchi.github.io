6c08b01e215725d1c8bb1bb36cd4d72176edb02b
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-10-30 17:41:29 -0400

    Moved skills into a seperate file.

--- src/home/SkillBar.js
+++ src/home/SkillBar.js
@@ -1,29 +1,15 @@
 import React from "react";
 import {
-  Button,
   Card,
   CardBody,
   CardHeader,
   CardText,
-  CardTitle,
   Col,
   Collapse,
-  Row,
-  UncontrolledCollapse
+  Row
 } from "reactstrap";
 import { LabeledSkillIcon, SkillIcon } from "./SkillIcon.js";
-import { Link } from "react-router-dom";
-import { CatBox } from "../CatBox.js";
-
-const skills = [
-  { icon: "fas fa-globe",      label: "Web Design",   color: "red" },
-  { icon: "fas fa-desktop",    label: "Desktop",      color: "blue" },
-  { icon: "fas fa-wrench",     label: "Tools",        color: "grey" },
-  { icon: "fas fa-flask",      label: "Research",     color: "purple" },
-  { icon: "fas fa-redo",       label: "Agile",        color: "black" },
-  { icon: "fas fa-dice-d20",   label: "Board Gaming", color: "green" },
-  { icon: "fas fa-ellipsis-h", label: "More",         color: "orange" }
-];
+import { skills } from "./skills.js";
 
 class SkillBar extends React.Component {
   constructor(props) {
---
+++ src/home/skills.js
@@ -0,0 +1,12 @@
+const skills = [
+  { icon: "fas fa-globe", label: "Web Design", color: "red" },
+  { icon: "fas fa-desktop", label: "Desktop", color: "blue" },
+  { icon: "fas fa-wrench", label: "Tools", color: "grey" }, 
+  { icon: "fas fa-flask", label: "Research", color: "purple" }, 
+  { icon: "fas fa-redo", label: "Agile", color: "black" }, 
+  { icon: "fas fa-dice-d20", label: "Board Gaming", color: "green" }, 
+  { icon: "fas fa-ellipsis-h", label: "More", color: "orange" }
+];
+
+
+export { skills };
