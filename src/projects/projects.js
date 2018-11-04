951fec628275f220c57ab77f1aee2020f1752d53
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-11-04 14:04:14 -0500

    Added Warp Racer project.

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CardText } from "reactstrap";
+import { CardText, CardImg } from "reactstrap";
 import { CodeBlock } from "./CodeBlock.js";
 
 const projects = [
@@ -67,6 +67,28 @@ for episode in episodes:
       </React.Fragment>
     )
   },
+
+  { id: "warpracer", title: "Warp Racer",
+    skills: [
+      { icon: "fab fa-node-js" ,label: "nodeJS", color: "darkolivegreen" },
+      { icon: "fab fa-js", label: "JavaScript", color: "orange" },
+      { icon: "fas fa-cubes", label: "THREE.js", color: "blue" },
+    ],
+    body: (
+      <React.Fragment>
+        <CardText>
+          Warp Racer is an in-progress multiplayer racing game built on the 
+          <span className="mono">space-dud</span> library. It allows multiple
+          users to connect to the server, select a racer, and race on
+          dynamically generated tracks in real-time.
+        </CardText>
+        <CardText>
+          Users can play using a keyboard, or mobile device.
+        </CardText>
+        <CardImg bottom width="100%" src="/images/warp_racer.jpg" />
+      </React.Fragment>
+    )
+  }
 ];
 
 export { projects };
