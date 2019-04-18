855b184cf93a5638d1eedb8fc6d84ace65e4f24b
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-04-17 20:25:29 -0400

    Caching of lint output.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
         test: /.jsx?$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
+        options: {
+          cache: true,
+        }
       },
       {
         test: /.jsx?$/,
