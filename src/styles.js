c77eac2a810ac08255e12bea1ed25a1ed009014f
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-04-27 10:58:55 -0400

    Changed FG color

--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,15 @@
+const fg_color = '#E9E7DA';
+
 const styles = {
   navbar: {
-    'border-bottom': '1px solid white',
+    'border-bottom': `1px solid${ fg_color }`,
     padding: 0,
   },
   text: {
     color: 'white',
   },
   home_icon: {
-    color: 'white',
+    color: fg_color,
     'font-weight': 500,
     padding: '10px',
   }
