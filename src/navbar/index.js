0e08b1796d398831e0bf54805b0e4a6bd8d563bf
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-04-27 10:59:10 -0400

    Removed unused import.

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '../styles.js';
 import { withStyles } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
