e08adf57b13059d09c550479e524456961c98fee
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-02 20:38:43 -0400

    Added animation to home icon.

--- src/navbar/HomeIcon.js
+++ src/navbar/HomeIcon.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
 import lockAspect from '../utils/lockAspect.js';
-import { fg_color } from '../styles.js';
+import { fg_color, bg_color } from '../styles.js';
 
 const styles = {
   home_icon_text: {
@@ -9,25 +9,70 @@ const styles = {
     'font-weight': 500,
     'padding': '10px',
     'font-size': 'xx-large',
+    'transition': 'color 0.2s',
   },
   home_icon: {
     'margin': '10px',
-    'border': '2px solid '+fg_color
+    'border': '2px solid '+fg_color,
+    'position': 'relative',
+  },
+  cover_left: {
+    'position': 'absolute',
+    'top': 0,
+    'left': 0,
+    'width': 0,
+    'height': 0,
+    'transition': 'width 0.5s, height 0.5s',
+    'background-color': fg_color,
+    'z-index': -1,
+  },
+  cover_right: {
+    'position': 'absolute',
+    'bottom': 0,
+    'right': 0,
+    'width': 0,
+    'height': 0,
+    'transition': 'width 0.5s, height 0.5s',
+    'background-color': fg_color,
+    'z-index': -1,
   }
 };
 
-const BaseHomeIcon = ({ classes }) => (
-  <div className={ classes.home_icon_text }>
+const BaseHomeIcon = ({ classes, hover }) => (
+  <div
+    className={ classes.home_icon_text }
+    style={ hover ? {
+      'color': bg_color
+    } : {} }>
     MM
   </div>
 );
 
 const InnerHomeIcon = ({ ...rest }) => (lockAspect(BaseHomeIcon, { ...rest }));
 
-const HomeIcon = ({ classes }) => (
-  <div className={ classes.home_icon }>
-    <InnerHomeIcon classes={ classes } />
-  </div>
+const Cover = ({ className, hover }) => (
+  <div
+    className={ className }
+    style={ hover ? {
+      'width': '100%',
+      'height': '100%',
+    } : {} }></div>
 );
+
+const HomeIcon = ({ classes }) => {
+  const [ hover, setHover ] = useState(false);
+  return (
+    <div
+      className={ classes.home_icon }
+      onMouseEnter={ () => { setHover(true); } }
+      onMouseLeave={ () => { setHover(false); } }>
+
+      <InnerHomeIcon classes={ classes } hover={ hover }/>
+
+      <Cover className={ classes.cover_left } hover={ hover } />
+      <Cover className={ classes.cover_right } hover={ hover } />
+    </div>
+  )
+};
 
 export default withStyles(styles)(HomeIcon);
