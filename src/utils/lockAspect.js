b1a28930cb627329a691886d48ddd6ca3931cb7e
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-02 20:56:15 -0400

    Added lockAspect function.

---
+++ src/utils/lockAspect.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Centers the element in an element with a 1:1 aspect ratio
+const lockAspect = (Element, { ...rest }) => (
+  <div style={{
+     display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center',
+  }} >
+    <div style={{
+      height: 0,
+      paddingTop: '100%',
+      width: '100%'
+    }}></div>
+    <Element { ...rest }/>
+  </div>
+);
+
+export default lockAspect;
