ef643949ef144d18d0068a827d11b08502963157
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-03 17:32:19 -0400

    Added splash image.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import NavBar from './navbar';
-import Grid from '@material-ui/core/Grid';
 import withStyles from '@material-ui/core/styles/withStyles.js';
 import Home from './home/';
 
 const styles = {
-  container: {
-    flexGrox: 1,
-  },
-  content: {
-    padding: '24px',
-  }
 };
 
 const App = ({ classes }) => (
   <Fragment>
     <NavBar />
 
-    <div className={ classes.container }>
-      <Grid container justify="center">
-        <Grid item xs={12} md={8} className={ classes.content }>
-          <Home />
-        </Grid>
-      </Grid>
+    <div>
+      <Home />
     </div>
   </Fragment>
 );
--- src/home/index.js
+++ src/home/index.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { Fragment } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
-import RightCol from './RightCol.js';
+import splash from './splash.jpg';
+import { color1, standard_shadow } from '../styles.js';
 
 const styles = {
-  column: {
-    textAlign: 'center'
+  splash: {
+    'display': 'flex',
+    'background-image': `url(${ splash })`,
+    'background-size': 'cover',
+    'background-position': 'bottom',
+    'align-items': 'center',
   },
-  root: {
-    'align-items': 'center'
+  greeting_col: {
+    'padding': '100px',
+  },
+  greeting_box: {
+    'color': color1,
+    'background': 'rgba(0, 0, 0, 0.7)',
+    'padding': '50px',
+    'box-shadow': standard_shadow,
+  },
+  greeting: {
+    'font-weight': 600,
+    'font-size': 'xx-large',
+    'padding-bottom': '2vh',
+  },
+  greeting_sub: {
+    'font-size': 'medium'
+  },
+  greeting_sub_sub: {
+    'font-size': 'small'
   }
 };
 
 const Home = ({ classes }) => (
-  <Grid container className={ classes.root }>
-    <Grid item xs={6} className={ classes.column }>
-      <h1>Max Mazzocchi</h1>
-    </Grid>
-    <RightCol />
-  </Grid>
+  <Fragment>
+    <div className={ classes.splash }>
+      <div className={ classes.greeting_col }>
+        <div className={ classes.greeting_box }>
+          <div className={ classes.greeting }>
+            Welcome.
+          </div>
+          <div className={ classes.greeting_sub }>
+            Max Mazzocchi, Software Developer
+          </div>
+          <div className={ classes.greeting_sub_sub }>
+            Washington DC/Northern Virginia
+          </div>
+        </div>
+      </div>
+    </div>
+  </Fragment>
 );
 
 export default withStyles(styles)(Home);
