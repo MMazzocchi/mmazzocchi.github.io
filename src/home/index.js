50fbc6b9a416fa01a3ec19b1b8e3a1910d5d9d04
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-05 08:55:49 -0400

    Began creating splash screen.

--- src/home/index.js
+++ src/home/index.js
@@ -5,10 +5,12 @@ import { color1, standard_shadow } from '../styles.js';
 
 const styles = {
   splash: {
-    'display': 'flex',
     'background-image': `url(${ splash })`,
     'background-size': 'cover',
-    'background-position': 'bottom',
+    'background-position': 'top',
+  },
+  header: {
+    'display': 'flex',
     'align-items': 'center',
   },
   greeting_col: {
@@ -25,23 +27,31 @@ const styles = {
     'font-size': 'xx-large',
     'padding-bottom': '2vh',
   },
+  content: {
+    'min-height': '1000px',
+  }
 };
 
 const Home = ({ classes }) => (
   <Fragment>
     <div className={ classes.splash }>
-      <div className={ classes.greeting_col }>
-        <div className={ classes.greeting_box }>
-          <div className={ classes.greeting }>
-            Welcome.
-          </div>
-          <div style={{ fontSize: 'medium' }}>
-            Max Mazzocchi, Software Developer
-          </div>
-          <div style={{ fontSize: 'small' }}>
-            Washington DC/Northern Virginia
+      <div className={ classes.header }>
+        <div className={ classes.greeting_col }>
+          <div className={ classes.greeting_box }>
+            <div className={ classes.greeting }>
+              Welcome.
+            </div>
+            <div style={{ fontSize: 'medium' }}>
+              Max Mazzocchi, Software Developer
+            </div>
+            <div style={{ fontSize: 'small' }}>
+              Washington DC/Northern Virginia
+            </div>
           </div>
         </div>
+      </div>
+      <div className={ classes.content }>
+        
       </div>
     </div>
   </Fragment>
