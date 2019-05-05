2302c80e19e9f963f657a7aa1e8fca0155aa0941
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-05 09:05:24 -0400

    Fixed position and size of splash.

--- src/home/index.js
+++ src/home/index.js
@@ -5,9 +5,15 @@ import { color1, standard_shadow } from '../styles.js';
 
 const styles = {
   splash: {
+    'position': 'absolute',
+    'top': 0,
+    'left': 0,
+    'width': '100%',
+    'min-height': '75%',
     'background-image': `url(${ splash })`,
     'background-size': 'cover',
-    'background-position': 'top',
+    'background-position': 'center',
+    'z-index': -1,
   },
   header: {
     'display': 'flex',
@@ -28,13 +34,17 @@ const styles = {
     'padding-bottom': '2vh',
   },
   content: {
-    'min-height': '1000px',
+  },
+  container: {
+    'position': 'relative',
   }
 };
 
 const Home = ({ classes }) => (
   <Fragment>
-    <div className={ classes.splash }>
+    <div className={ classes.container }>
+      <div className={ classes.splash }>
+      </div>
       <div className={ classes.header }>
         <div className={ classes.greeting_col }>
           <div className={ classes.greeting_box }>
