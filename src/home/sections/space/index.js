d21a5572c48eaf71a490e2b3dc5e5c0707724513
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-05 21:42:13 -0400

    Moved solar system to new file.

--- src/home/sections/space/index.js
+++ src/home/sections/space/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
 import Section from '../Section.js';
-import { color2, color3 } from '../../../styles.js';
+import { color3 } from '../../../styles.js';
+import SolarSystem from './SolarSystem.js';
 
 const styles = {
   title: {
@@ -9,53 +10,9 @@ const styles = {
     'font-size': 'xx-large',
   },
   solar_system: {
-    'min-width': '200px',
-    'min-height': '200px',
-    'position': 'relative',
-    'margin': '20px',
-  },
-  planet_container: {
-    'position': 'absolute',
-    'top': 0,
-    'left': 0,
-    'width': '100%',
-    'height': '100%',
-  },
-  planet: {
-    'padding': '5px',
-    'background-color': color2,
-    'border-radius': '5px',
-  },
-  sun: {
-    'padding': '15px',
-    'border-radius': '15px',
-    'background-color': color2,
-  },
-  body: {
-    'position': 'absolute',
-    'width': 0,
-    'height': 0,
-    'display': 'flex',
-    'align-items': 'center',
-    'justify-content': 'center',
-    'top': '50%',
-    'right': '50%',
-  },
+    'padding-left': '20px',
+  }
 };
-
-const Sun = ({ classes }) => (
-  <div className={ classes.body }>
-    <div className={ classes.sun }></div>
-  </div>
-);
-
-const Planet = ({ classes, x, y }) => (
-  <div className={ classes.planet_container }>
-    <div className={ classes.body } style={{ top: y, left: x }}>
-      <div className={ classes.planet }></div>
-    </div>
-  </div>
-);
 
 const Space = ({ classes }) => (
   <Section>
@@ -71,11 +28,7 @@ const Space = ({ classes }) => (
       </div>
     </div>
     <div className={ classes.solar_system }>
-      <Sun classes={ classes } />
-      <Planet classes={ classes } x="10%" y="50%" />
-      <Planet classes={ classes } x="18%" y="50%" />
-      <Planet classes={ classes } x="26%" y="50%" />
-      <Planet classes={ classes } x="34%" y="50%" />
+      <SolarSystem />
     </div>
   </Section>
 );
