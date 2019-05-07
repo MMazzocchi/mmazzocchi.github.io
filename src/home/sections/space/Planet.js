7ab20095f01f9df84da54915a9d9f17956b0251e
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-07 06:21:17 -0400

    Moved Planet to new file.

--- src/home/sections/space/Planet.js
+++ src/home/sections/space/Planet.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
 import { color4 } from '../../../styles.js';
-import Planet from './Planet.js';
 import './orbit.css';
 
 const styles = {
-  solar_system: {
-    'min-width': '200px',
-    'min-height': '200px',
-    'position': 'relative',
+  planet_container: {
+    'position': 'absolute',
+    'top': 0,
+    'left': 0,
+    'width': '100%',
+    'height': '100%',
+    'display': 'flex',
+    'align-items': 'center',
+    'justify-content': 'center',
   },
   orbit_container: {
     'position': 'absolute',
@@ -19,6 +23,12 @@ const styles = {
     'display': 'flex',
     'align-items': 'center',
     'justify-content': 'center',
+  },
+  orbit: {
+    'border': '1px '+color4+' solid',
+    'width': 0,
+    'height': 0,
+    'border-radius': '50%',
   },
   body_container: {
     'position': 'absolute',
@@ -33,10 +43,10 @@ const styles = {
     'align-items': 'center',
     'justify-content': 'center',
   },
-  sun: {
-    'padding': '15px',
-    'border-radius': '15px',
+  planet: {
+    'padding': '5px',
     'background-color': color4,
+    'border-radius': '5px',
   },
   body: {
     'position': 'absolute',
@@ -50,20 +60,26 @@ const styles = {
   },
 };
 
-const Sun = ({ classes }) => (
-  <div className={ classes.body }>
-    <div className={ classes.sun }></div>
+const Planet = ({ classes, x, y, orbit_radius, duration }) => (
+  <div
+    className={ classes.planet_container }
+    >
+
+    <div className={ classes.orbit_container }>
+      <div
+        className={ classes.orbit }
+        style={{
+          'padding': orbit_radius,
+        }}></div>
+    </div>
+
+    <div className={ classes.body_container }
+      style={{ animationDuration: duration }}>
+      <div className={ classes.body } style={{ top: y, left: x }}>
+        <div className={ classes.planet }></div>
+      </div>
+    </div>
   </div>
 );
 
-const SolarSystem = ({ classes }) => (
-  <div className={ classes.solar_system }>
-    <Sun classes={ classes } />
-    <Planet classes={ classes } orbit_radius="40%" y="10%" x="50%" duration="27s" />
-    <Planet classes={ classes } orbit_radius="32%" y="18%" x="50%" duration="9s" />
-    <Planet classes={ classes } orbit_radius="24%" y="26%" x="50%" duration="3s" />
-    <Planet classes={ classes } orbit_radius="16%" y="34%" x="50%" duration="1s" />
-  </div>
-);
-
-export default withStyles(styles)(SolarSystem);
+export default withStyles(styles)(Planet);
