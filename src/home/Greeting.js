7ee333a874be763db3eb4a41137c6a5c869e1dc2
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-07 19:30:51 -0400

    Added Contact button.

--- src/home/Greeting.js
+++ src/home/Greeting.js
@@ -2,8 +2,11 @@ import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
 import {
   color1,
+  color2,
+  color2_light,
   color3,
   standard_shadow,
+  elevated_shadow,
   semi_transparent
 } from '../styles.js';
 
@@ -19,6 +22,24 @@ const styles = {
     'font-size': 'xx-large',
     'padding-bottom': '2vh',
   },
+  button: {
+    'background': color2,
+    'font-weight': 600,
+    'text-align': 'center',
+    'padding': '10px',
+    'box-shadow': standard_shadow,
+    'display': 'inline-block',
+    'border-radius': '5px',
+    'transition': 'background 0.2s, box_shadow 0.2s',
+    '&:hover': {
+      'background': color2_light,
+      'box_shadow': elevated_shadow,
+    }
+  },
+  link: {
+    'text-decoration': 'none',
+    'color': color1,
+  }
 };
 
 const Greeting = ({ classes }) => (
@@ -29,8 +50,13 @@ const Greeting = ({ classes }) => (
     <div style={{ fontSize: 'medium' }}>
       Max Mazzocchi, Software Developer
     </div>
-    <div style={{ fontSize: 'small' }}>
+    <div style={{ fontSize: 'small', paddingBottom: '10px', }}>
       Washington DC/Northern Virginia
+    </div>
+    <div className={ classes.button }>
+      <a className={ classes.link } href="mailto://maxwell.mazzocchi@gmail.com">
+        Contact Me
+      </a>
     </div>
   </div>
 );
--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,22 @@
 const color1 = '#F4F3EC';
 const color2 = '#286DA8';
+const color2_light = '#2C78B9';
 const color3 = '#2F2F2F';
 const color4 = '#C41E3D';
 const color5 = '#551C41';
 
 const standard_shadow = '0 0 8px 0 '+color3;
+const elevated_shadow = '0 0 12px 0 '+color3;
 const semi_transparent = 'E4';
 
-export { color1, color2, color3, color4, color5, standard_shadow, semi_transparent };
+export {
+  color1,
+  color2,
+  color2_light,
+  color3,
+  color4,
+  color5,
+  standard_shadow,
+  elevated_shadow,
+  semi_transparent
+};
