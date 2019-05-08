ab05f3d0816edd03d4ee3e543be0f484419d1f23
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-07 23:52:08 -0400

    Made Section switch to column mode on small screens.

--- src/home/sections/Section.js
+++ src/home/sections/Section.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles.js';
-import { color3 } from '../../styles.js';
+import { color3, breakpoints } from '../../styles.js';
 
 const styles = {
   section: {
@@ -12,13 +12,30 @@ const styles = {
     'margin-top': '30px',
     'padding-bottom': '30px',
   },
+  section_reverse: {
+    'display': 'flex',
+    'align-items': 'center',
+    'justify-content': 'space-between',
+    'margin-left': '50px',
+    'margin-right': '50px',
+    'margin-top': '30px',
+    'padding-bottom': '30px',
+    'flex-direction': 'row-reverse'
+  },
+  [`@media (max-width: ${ breakpoints.md })`]: {
+    section: {
+      'flex-direction': 'column',
+    },
+    section_reverse: {
+      'flex-direction': 'column',
+    }
+  }
 };
 
 const Section = ({ classes, children, reverse, last }) => (
   <div
-    className={ classes.section }
+    className={ reverse ? classes.section_reverse : classes.section }
     style={{
-      flexDirection: reverse ? 'row-reverse' : '',
       borderBottom: last ? '' : '1px'+color3+' solid',
      }}>
     { children }
