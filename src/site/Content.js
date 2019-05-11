ba25b6dfe75c7dda2180b8dad03d29eaa61db28b
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-11 10:55:10 -0400

    Fixed Content resizing issues.

--- src/site/Content.js
+++ src/site/Content.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { color1, standard_shadow } from './styles.js';
+import { color1, standard_shadow, breakpoints } from './styles.js';
 import withStyles from 'react-jss';
 
 const styles = {
   content: {
     'background-color': color1,
     'box-shadow': standard_shadow,
-    'width': '90%',
     'max-width': '1000px',
     'padding-left': '50px',
     'padding-right': '50px',
     'padding-top': '30px',
     'padding-bottom': '30px',
+    'margin-left': '30px',
+    'margin-right': '30px'
   },
   container: {
     'position': 'relative',
