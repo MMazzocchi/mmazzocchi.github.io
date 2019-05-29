eee40045dc1b50637034b9cc9b8fb8612ca38f80
Maxwell Mazzocchi <maxwell.mazzocchi@gmail.com>
2019-05-29 18:16:03 -0400

    Moved Content CSS into index.

--- src/site/index.js
+++ src/site/index.js
@@ -3,12 +3,12 @@ import NavBar from './navbar';
 import withStyles from 'react-jss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import Header from './header';
-import Content from './Content.js';
 import Home from './home';
 import Contact from './contact';
 import Footer from './Footer.js';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Backgrounds from './Backgrounds.js';
+import { color1, standard_shadow } from './styles.js';
 
 const styles = {
   container: {
@@ -17,6 +17,19 @@ const styles = {
     'display': 'flex',
     'flex-direction': 'column',
   },
+  content: {
+    'background-color': color1,
+    'box-shadow': standard_shadow,
+    'max-width': '1000px',
+    'padding-left': '50px',
+    'padding-right': '50px',
+    'padding-top': '30px',
+    'padding-bottom': '30px',
+    'margin-left': '30px',
+    'margin-right': '30px',
+    'flex-grow': 1,
+    'justify-content': 'center',
+  },
 };
 
 const Site = ({ classes }) => (
@@ -24,17 +37,15 @@ const Site = ({ classes }) => (
 
     <div className={ classes.container }>
       <Backgrounds />
-
       <NavBar />
-
       <Header />
-      <Content>
+
+      <div className={ classes.content }>
         <Route exact path="/" component={ Home } />
         <Route path="/contact" component={ Contact } />
-      </Content>
-      <Footer />
+      </div>
 
-      <div className={ classes.bottom_splash }></div>
+      <Footer />
     </div>
   </Router>
 );
